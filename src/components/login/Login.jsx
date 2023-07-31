fcbfc9fc96ee35aa2f4fc929e20dd9c968cd08d1
thisistanzeela <tanzeelamansoor99@gmail.com>
2023-07-31 23:31:04 +0530

    final commit

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,5 +1,109 @@
+// import React, { useState, useEffect } from 'react';
+// import '../login/Profile.css';
+// import Welcome from './Welcome';
+// import Profile from './Profile';
+// import { useLocalStorage } from '../useLocalStorage';
+
+// export default function Login(props) {
+//   const [home, setHome] = useState(true);
+//   const [userName, setUserName] = useState('');
+//   const [password, setPassword] = useState('');
+//   const [usersData, setUsersData] = useState(null);
+//   const [loginInfo, setLoginAInfo] = useLocalStorage('userInfo', []);
+
+//   useEffect(() => {
+//     const apicalluser = async () => {
+//       const response = await fetch(
+//         "https://meesho-d82c7-default-rtdb.firebaseio.com/userdata.json"
+//       );
+//       const data = await response.json();
+//       const newdata = Object.values(data);
+//       setUsersData(newdata);
+//     };
+//     apicalluser();
+//   }, []);
+
+//   const submitHandler = (e) => {
+//     e.preventDefault();
+
+//     if (usersData) {
+//       const userData = Object.values(usersData);
+//       const matchedUser = userData.find((user) => user.userName === userName);
+
+//       if (!matchedUser) {
+//         alert("User not found");
+//       } else {
+//         const userPassword = matchedUser.userpassword;
+//         const verify = userPassword === password;
+//         if (verify) {
+//           alert("Login successful");
+//           // Update the state to switch to the welcome screen
+//           setLoginAInfo(matchedUser.userName);
+//           setHome(true);
+//         } else {
+//           alert("Invalid password");
+//         }
+//       }
+//     } else {
+//       alert("Data not available");
+//     }
+//   };
+
+//   const userInputHandler = (e) => {
+//     setUserName(e.target.value);
+//   };
+
+//   const passwordInputHandler = (e) => {
+//     setPassword(e.target.value);
+//   };
+
+//   const logoutHandler = () => {
+//     setHome(true); // Set home state to true to show the login form again
+//     setUserName(''); // Clear the username
+//     setPassword(''); // Clear the password
+   
+//   };
+
+//   return (
+//     <div>
+//       {home ? (
+//         <div className='login-container'>
+//           <img src="https://images.meesho.com/images/marketing/1661417516766.webp" alt="sign up img" style={{ height: "auto", width: '400px' }} />
+//           <form className="logIn" onSubmit={submitHandler}>
+//             <label htmlFor="">UserName:</label>
+//             <input onChange={userInputHandler} type="text" placeholder="Username" />
+//             <label htmlFor="">Password</label>
+//             <input type="password" onChange={passwordInputHandler} placeholder="Password" />
+//             <button type="submit" className='loginBtn'>Login</button>
+//           </form>
+//           <button className='haveAccBtn'
+//             style={{ width: '100%' }}
+//             onClick={() => {
+//               props.toggleForm();
+//             }}
+//           >
+//             Don't have an account?
+//           </button>
+//         </div>
+//       ) : (
+//         // <Welcome userName={userName} onLogout={logoutHandler} />
+//         <Profile userName={userName} stateCallBack={() => setHome(true)} />
+//       )}
+     
+//     </div>
+//   );
+// }
+
+
+
+
+
+
+
+
 import React, { useState, useEffect } from 'react';
 import '../login/Profile.css';
+import Welcome from './Welcome';
 import Profile from './Profile';
 import { useLocalStorage } from '../useLocalStorage';
 
@@ -8,7 +112,7 @@ export default function Login(props) {
   const [userName, setUserName] = useState('');
   const [password, setPassword] = useState('');
   const [usersData, setUsersData] = useState(null);
-  const [loginInfo, setLoginAInfo] = useLocalStorage('userInfo', []);
+  const [loginInfo, setLoginAInfo] = useLocalStorage('userInfo', null); // Initialize with null instead of an empty array
 
   useEffect(() => {
     const apicalluser = async () => {
@@ -60,6 +164,7 @@ export default function Login(props) {
     setHome(true); // Set home state to true to show the login form again
     setUserName(''); // Clear the username
     setPassword(''); // Clear the password
+    setLoginAInfo(null); // Clear the userInfo from localStorage after logout
   };
 
   return (
@@ -85,8 +190,8 @@ export default function Login(props) {
         </div>
       ) : (
         // <Welcome userName={userName} onLogout={logoutHandler} />
-        <Profile userName={userName} stateCallBack={() => setHome(true)} />
-      )}
+        <Profile userName={loginInfo} stateCallBack={() => setHome(true)} />
+        )}
      
     </div>
   );
--- src/components/login/Profile.jsx
+++ src/components/login/Profile.jsx
@@ -1,3 +1,41 @@
+// import React from 'react';
+// import { FaUser } from 'react-icons/fa';
+// import '../login/Profile.css';
+
+// export default function Profile(props) {
+
+//   const userName = JSON.parse(localStorage.getItem('userInfo'));
+
+//   const loginCondition = () => {
+//     props.stateCallBack();
+//   };
+//   const logoutHandler = () => {
+//     localStorage.removeItem('userInfo');
+//     props.stateCallBack();
+//   };
+
+//   return (
+//     <div className="profileBar">
+//       {userName ? (
+//         <div className="log-container">
+//            <h3 style={{marginTop:'18px', fontSize:'24px'}}>  <FaUser className="user-icon" style={{marginRight:'10px',marginLeft:'12px', color:'purple'}}/>{userName}</h3>
+//           <hr className='profile-hr'/>
+//           <button className="logout-btn" onClick={logoutHandler}>Log Out</button>
+//         </div>
+//       ) : (
+//         <div className="log-container">
+//           <h3>Hello User!!</h3>
+//           <p>To access your Meesho account</p>
+//           <button className="SignUpBtn" onClick={loginCondition}>
+//             SignUp
+//           </button>
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
+
+
 import React from 'react';
 import { FaUser } from 'react-icons/fa';
 import '../login/Profile.css';
@@ -18,13 +56,12 @@ export default function Profile(props) {
     <div className="profileBar">
       {userName ? (
         <div className="log-container">
-           <h3 style={{marginTop:'18px', fontSize:'24px'}}>  <FaUser className="user-icon" style={{marginRight:'10px',marginLeft:'12px', color:'purple'}}/>{userName}</h3>
+           <h3 className='login-h3'>  <FaUser className="user-icon" />{userName}</h3>
           <hr className='profile-hr'/>
           <button className="logout-btn" onClick={logoutHandler}>Log Out</button>
         </div>
       ) : (
         <div className="log-container">
-          <img src="path_to_your_logo.png" alt="Logo" className="logo" />
           <h3>Hello User!!</h3>
           <p>To access your Meesho account</p>
           <button className="SignUpBtn" onClick={loginCondition}>
@@ -35,5 +72,3 @@ export default function Profile(props) {
     </div>
   );
 }
-
-
